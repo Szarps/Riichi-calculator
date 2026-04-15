ef45a1b185a45a44c9a8427bc271e9bf2dc21acb
Szarps <szarps@hotmail.com>
2026-04-14 23:04:19 -0300

    Adjusted values and functions for new structure
    Some files like calculate_fu.py were working only halfway through.
    Now is more deterministic and precise, still needs some features but
    the base is there and some notations for later reference.
    
    Modified test_calculate_fu since it was outdated for it's purpose. For
    the time being is put slightly on hold until other more core parts take
    form.
    
    Updated pyproject.toml to reflect version and added UV for management.

--- src/application/tiles.py
+++ src/application/tiles.py
@@ -3,16 +3,16 @@
 # ===Libs===
 
 # ===Modules===
-# from dataclasses import dataclass
 
+# The class TileTypes might be unnecesary and could benefit of becoming a list
 ########################################
 
 class TileTypes:
     simple: tuple = (2, 3, 4, 5, 6, 7, 8)
     terminal: tuple = (1, 9)
-    kind: tuple = ("b", "m", "p")
-    dragon: tuple = ("R", "H", "G")
-    wind: tuple = ("E", "S", "W", "N")
+    kind: tuple = ("BAMBOO", "MAN", "PIN")
+    dragon: tuple = ("CHUN", "HAKU", "HATSU")
+    wind: tuple = ("TON", "NAN", "SHAA", "PEI")
     tile_type: dict = {"simple": False, "terminal": False, "honor": False, "wind": False, "dragon": False}
 
 
@@ -24,12 +24,10 @@ class Tile:
             self.value = int(args[0])
             if self.value in [i for i in TileTypes.terminal]:
                 self.kind = args[1]
-                self.tile_type = "terminal"
-                print("terminal")
+                self.tile_type = {"terminal": True}
             else:
                 self.kind = args[1]
-                self.tile_type = "simple"
-                print("simple")
+                self.tile_type = {"simple": True}
 
         elif args[0] in TileTypes.dragon:
              self.tile_type = {"dragon": True, "honor": True}
@@ -39,5 +37,5 @@ class Tile:
 
 
 if __name__ == '__main__':
-    tile1 = Tile("2p")
+    tile1 = Tile("9p")
     print(tile1.value, tile1.kind if hasattr(tile1, "kind") else "", tile1.tile_type)
